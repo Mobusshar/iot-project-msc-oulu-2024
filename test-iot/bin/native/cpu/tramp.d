0cd01c6697a4308ab0c4e21d346a42bcf9f078cb
Md Mobusshar Islam <mi.tonmoy4@gmail.com>
2024-01-28 00:41:26 +0200

    Update

--- test-iot/bin/native/cpu/tramp.d
+++
@@ -1,8 +0,0 @@
-/senslab/users/iot2023oulu15/test-iot/bin/native/cpu/tramp.o: \
- /senslab/users/iot2023oulu15/RIOT/cpu/native/tramp.S \
- /usr/include/stdc-predef.h \
- /senslab/users/iot2023oulu15/test-iot/bin/native/riotbuild/riotbuild.h
-
-/usr/include/stdc-predef.h:
-
-/senslab/users/iot2023oulu15/test-iot/bin/native/riotbuild/riotbuild.h:
